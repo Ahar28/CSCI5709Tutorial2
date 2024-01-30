b1737109c8dc2688b71c20e753e91b3ef22c7ee3
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2024-01-29 22:40:22 -0400

    initial app

---
+++ tutorial2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Aharnish B00933563</h1>
+        <p>
+          It Works
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
