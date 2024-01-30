fcb5e6b8a79348dd2ff3033002df59b9a61fc9a1
Aharnish Maheshbhai Solanki <ah910744@dal.ca>
2024-01-29 23:46:12 -0400

    tutorial 2 done

--- tutorial2/src/App.js
+++ tutorial2/src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Aharnish B00933563</h1>
+        <h1>Tutorial 2</h1>
+        <h2>Aharnish B00933563</h2>
         <p>
-          It Works
+          It Works !!
         </p>
       </header>
     </div>
